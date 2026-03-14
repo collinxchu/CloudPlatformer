734c70d5c26949486be97140d5eb4c36b26cae42
Jumbowl <collinxchu@gmail.com>
2026-03-14 03:46:17 -0300

    Fiddled with jumps and shit
    Movement still needs update to be per-frame instead of just when the Move action is called, but it is far too early in the morning lmao.

--- CloudPlatformer/Assets/PlayerController.cs
+++ CloudPlatformer/Assets/PlayerController.cs
@@ -2,29 +2,50 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-  PlayerInput playerInput;
-
-  [Header("References")]
-  public Rigidbody collider;
-  public Transform cameraAnchor;
-  public Camera camera;
-
-  [Header("Variables")]
-  public float charSpeed;
-  public float charJumpHeight;
-
-    
+	PlayerInput playerInput;
+	int jumpCount;
+	
+	[Header("References")]
+	public Rigidbody collider;
+	public Transform cameraAnchor;
+	public Camera camera;
+	public Text debug;
+	
+	[Header("Variables")]
+	public float charSpeed = 10;
+	public float charJumpHeight = 6;
+	public int charJumps = 1;
+	
+	float distToGround = 1f;
+	
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+		// Enable (default) action set.
         playerInput = GetComponent<PlayerInput>();
         InputSystem.actions.Enable();
         playerInput.currentActionMap?.Enable();
     }
-   
+	
+	public bool IsGrounded()
+	{
+		if (Physics.Raycast(transform.position, Vector3.down, distToGround + 0.01f))
+		{
+			Debug.Log("Grounded");
+			return true;
+		}
+		else
+		{
+			Debug.Log("Not grounded");
+			return false;
+		}
+	}
+	
+	// Not called if the 'move' state hasn't changed. TODO: Fix.
     public void OnMove(InputValue value){
         var charMove = value.Get<Vector2>();
         Vector3 newSpeed = Vector3.up * collider.linearVelocity.y;
@@ -32,17 +53,31 @@ public class PlayerController : MonoBehaviour
         newSpeed.z = charMove.y * charSpeed;
         collider.linearVelocity = newSpeed;
     }
-
+	
     public void OnJump(){
-		Vector3 newSpeed = Vector3.zero;
-		newSpeed.y += charJumpHeight;
+		
         if(playerInput.actions["Jump"].WasPressedThisFrame())
-            collider.linearVelocity = newSpeed;
+			if ((IsGrounded()) || (jumpCount > 0))
+			{
+				Jump();
+			}
     }
+	
+	void Jump() {
+		collider.linearVelocity = new Vector3(collider.linearVelocity.x, charJumpHeight, collider.linearVelocity.z);
+		--jumpCount;
+	}
 
     // Update is called once per frame
     void Update()
     {
         
     }
+	
+	void FixedUpdate() {
+		if (IsGrounded())
+		{
+			jumpCount = charJumps;
+		}
+	}
 }
